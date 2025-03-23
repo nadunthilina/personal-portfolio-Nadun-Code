4c3b94b51d7a42bd637631044b457858aeaf2f01
Nadun Thilina <152951157+nadunthilina@users.noreply.github.com>
2025-03-23 19:55:09 +0530

    sticky navbar & toggle icon navbar

--- script.js
+++ script.js
@@ -78,3 +78,23 @@ function activeMenu(){
 activeMenu();
 window.addEventListener("scroll",activeMenu);
 
+// sticky navbar /////////////////////////////////////////////////
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
+// toggle icon navbar ////////////////////////////////////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
